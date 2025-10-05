ca62c97f05671881e54bfb9df00fe21944330dce
Давид Алиев <1davidios1@gmail.com>
2025-10-05 18:44:48 +0300

    fix(eslint): правка линтера, отказ от prettier

--- client/components/admin/editModal.vue
+++ client/components/admin/editModal.vue
@@ -1,137 +1,137 @@
 <script setup>
-import { useCategoriesStore } from '#imports';
+    import { useCategoriesStore } from '#imports';
 
-import { VsNotification } from 'vuesax-alpha';
-import { VFileUpload } from 'vuetify/labs/VFileUpload';
+    import { VsNotification } from 'vuesax-alpha';
+    import { VFileUpload } from 'vuetify/labs/VFileUpload';
 
-const props = defineProps({
-    modelValue: {
-        type: Object,
-        default: () => {},
-    },
-    visible: {
-        type: Boolean,
-        default: false,
-    },
-});
-const emit = defineEmits(['update:visible', 'update:model-value', 'updateProduct']);
-const categoriesStore = useCategoriesStore();
-const config = useRuntimeConfig();
-const { $api } = useNuxtApp();
-const loading = ref(false);
-const item = ref({ ...props.modelValue });
-const category = ref(item.value.productSubcategory?.productCategory.id);
-const subcategory = ref(item.value.productSubcategory);
+    const props = defineProps({
+        modelValue: {
+            type: Object,
+            default: () => {},
+        },
+        visible: {
+            type: Boolean,
+            default: false,
+        },
+    });
+    const emit = defineEmits(['update:visible', 'update:model-value', 'updateProduct']);
+    const categoriesStore = useCategoriesStore();
+    const config = useRuntimeConfig();
+    const { $api } = useNuxtApp();
+    const loading = ref(false);
+    const item = ref({ ...props.modelValue });
+    const category = ref(item.value.productSubcategory?.productCategory.id);
+    const subcategory = ref(item.value.productSubcategory);
 
-async function updateImage(image) {
-    const formData = new FormData();
-    formData.append('image', image, image.name);
-    formData.append('productArticleId', item.value.id);
-    loading.value = true;
-    await $api('/api/product-article/admin/image', { method: 'POST', body: formData }).then(() => {
-        loading.value = false;
-        VsNotification({
-            title: 'Отлично!',
-            content: 'Картинка была загружена',
-            position: 'bottom-center',
-            border: 'success',
+    async function updateImage(image) {
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        formData.append('productArticleId', item.value.id);
+        loading.value = true;
+        await $api('/api/product-article/admin/image', { method: 'POST', body: formData }).then(() => {
+            loading.value = false;
+            VsNotification({
+                title: 'Отлично!',
+                content: 'Картинка была загружена',
+                position: 'bottom-center',
+                border: 'success',
+            });
+            getProduct();
+        }).catch(() => {
+            VsNotification({
+                title: 'Ошибка!',
+                content: 'Ошибка загрузки фото',
+                position: 'bottom-center',
+                border: 'danger',
+            });
         });
+    };
+
+    function handleImageError(event) {
+        console.error('Ошибка загрузки изображения в админке:', event.target.src);
+    }
+
+    async function updateProduct() {
+        await $api('/api/product-article/admin/productArticle', {
+            method: 'PATCH',
+            body: { productArticleId: item.value.id, ...item.value },
+        }).then(() => {
+            loading.value = false;
+            VsNotification({
+                title: 'Отлично!',
+                content: 'Продукт был обновлён',
+                position: 'bottom-center',
+                border: 'success',
+            });
+            getProduct();
+            emit('updateProduct');
+        }).catch(() => {
+            getProduct();
+            VsNotification({
+                title: 'Ошибка!',
+                content: 'Продукт не был обновлён',
+                position: 'bottom-center',
+                border: 'danger',
+            });
+        });
+    };
+
+    function deleteImage(productFileId) {
+        $api('/api/product-article/admin/image', { method: 'DELETE', body: { productFileId } }).then(() => {
+            loading.value = false;
+            VsNotification({
+                title: 'Отлично!',
+                content: 'Изображение было удалено',
+                position: 'bottom-center',
+                border: 'success',
+            });
+            getProduct();
+        });
+    }
+
+    function changeLogo(productFileId) {
+        $api('/api/product-article/admin/image', { method: 'PATCH', body: { productFileId } }).then(() => {
+            loading.value = false;
+            getProduct();
+            VsNotification({
+                title: 'Отлично!',
+                content: 'Логотип обновлён',
+                position: 'bottom-center',
+                border: 'success',
+            });
+        });
+    }
+
+    function bindSubcategory(id) {
+        $api('/api/product-category/subcategory/bindProduct', { method: 'POST', body: {
+            productArticleId: item.value.id,
+            subcategoryId: id,
+        } }).then(() => {
+            loading.value = false;
+            VsNotification({
+                title: 'Отлично!',
+                content: 'Категория обновлена',
+                position: 'bottom-center',
+                border: 'success',
+            });
+        });
+    }
+
+    async function getProduct() {
+        await $api(`/api/product-article/admin/${item.value.id}`, { method: 'POST' }).then((res) => {
+            loading.value = false;
+            item.value = res;
+            category.value = item.value.productSubcategory?.productCategory.id;
+            subcategory.value = item.value.productSubcategory;
+        });
+    }
+
+    watch(() => props.visible, async (value) => {
+        if (!value)
+            return;
+        item.value = props.modelValue;
         getProduct();
-    }).catch(() => {
-        VsNotification({
-            title: 'Ошибка!',
-            content: 'Ошибка загрузки фото',
-            position: 'bottom-center',
-            border: 'danger',
-        });
     });
-};
-
-function handleImageError(event) {
-    console.error('Ошибка загрузки изображения в админке:', event.target.src);
-}
-
-async function updateProduct() {
-    await $api('/api/product-article/admin/productArticle', {
-        method: 'PATCH',
-        body: { productArticleId: item.value.id, ...item.value },
-    }).then(() => {
-        loading.value = false;
-        VsNotification({
-            title: 'Отлично!',
-            content: 'Продукт был обновлён',
-            position: 'bottom-center',
-            border: 'success',
-        });
-        getProduct();
-        emit('updateProduct');
-    }).catch(() => {
-        getProduct();
-        VsNotification({
-            title: 'Ошибка!',
-            content: 'Продукт не был обновлён',
-            position: 'bottom-center',
-            border: 'danger',
-        });
-    });
-};
-
-function deleteImage(productFileId) {
-    $api('/api/product-article/admin/image', { method: 'DELETE', body: { productFileId } }).then(() => {
-        loading.value = false;
-        VsNotification({
-            title: 'Отлично!',
-            content: 'Изображение было удалено',
-            position: 'bottom-center',
-            border: 'success',
-        });
-        getProduct();
-    });
-}
-
-function changeLogo(productFileId) {
-    $api('/api/product-article/admin/image', { method: 'PATCH', body: { productFileId } }).then(() => {
-        loading.value = false;
-        getProduct();
-        VsNotification({
-            title: 'Отлично!',
-            content: 'Логотип обновлён',
-            position: 'bottom-center',
-            border: 'success',
-        });
-    });
-}
-
-function bindSubcategory(id) {
-    $api('/api/product-category/subcategory/bindProduct', { method: 'POST', body: {
-        productArticleId: item.value.id,
-        subcategoryId: id,
-    } }).then(() => {
-        loading.value = false;
-        VsNotification({
-            title: 'Отлично!',
-            content: 'Категория обновлена',
-            position: 'bottom-center',
-            border: 'success',
-        });
-    });
-}
-
-async function getProduct() {
-    await $api(`/api/product-article/admin/${item.value.id}`, { method: 'POST' }).then((res) => {
-        loading.value = false;
-        item.value = res;
-        category.value = item.value.productSubcategory?.productCategory.id;
-        subcategory.value = item.value.productSubcategory;
-    });
-}
-
-watch(() => props.visible, async (value) => {
-    if (!value)
-        return;
-    item.value = props.modelValue;
-    getProduct();
-});
 </script>
 
 <template>
