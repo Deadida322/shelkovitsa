f79c5405b4b4cef0b1da08ce1532e3ddbf09e465
Тимофей Кочетков <pochta30052001@gmail.com>
2025-10-05 19:40:13 +0400

    fix server types

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,16 @@
+// Глобальные типы для Express
+declare namespace Express {
+  namespace Multer {
+    interface File {
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      size: number;
+      destination: string;
+      filename: string;
+      path: string;
+      buffer: Buffer;
+    }
+  }
+}
